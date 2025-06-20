8f3f2eeafb024b1b9f556dc8c64bffeca3ca77ea
negai <negaimarin@gmail.com>
2025-06-20 18:53:05 +0300

    changed return.py

--- lectia_11/return.py
+++ lectia_11/return.py
@@ -26,7 +26,7 @@ def impartire_sigura(a, b):
 
 # Demonstrații
 print("1. Return condiționat:")
-print(f"5 este {verifica_paritate(5)}")
+print(f"8 este {verifica_paritate(8)}")
 print(f"6 este {verifica_paritate(6)}")
 
 print("\n2. Return multiple:")
